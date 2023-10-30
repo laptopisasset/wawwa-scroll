d68d231c853ed7b7e7d21f4f23688ddb12799471
Soundhar Natarajan <soundrnr@amazon.com>
2023-10-30 21:13:31 +0530

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import {Canvas} from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import './App.css';
 import { Experience } from "./components/Experience";
 
 function App() {
 
   return (
-    <Canvas camera={{position: [3, 3, 3], fov: 30}}>
-      <color attach="background" args={["#ececec"]}/>
+    <Canvas camera={{ position: [3, 3, 3], fov: 30 }}>
+      <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
   )
